37f1a6e46a9a87fa91530567af0ec624701aafb1
Jinal Jain <jinaljain624@gmail.com>
2025-05-01 17:03:47 +0530

    commit

---
+++ Looping and Patterns/Prg1.cpp
@@ -0,0 +1,11 @@
+// WAP to print number from 1 to 10 using for loop.
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
