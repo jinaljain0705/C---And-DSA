215145b1b50632db5e5ba2be6bb32fde44da8b8a
Jinal Jain <jinaljain624@gmail.com>
2025-05-07 14:22:43 +0530

    commit

---
+++ Pattern/Pttrn04.cpp
@@ -0,0 +1,23 @@
+/*
+    1
+    1 2
+    1 2 3
+    1 2 3 4 
+    1 2 3 4 5
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int i,j;
+    cout <<"enter the number:";
+    cin >> n;
+    for(i=1; i<n; i++){
+        for(j=1; j<=i; j++){
+            cout << j << " ";        
+        }
+    }
+        cout << endl;
+    return 0;
+}
