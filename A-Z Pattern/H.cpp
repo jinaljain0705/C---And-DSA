a059a495b9034122607aac19894f240a1a2c3dbe
Jinal Jain <jinaljain624@gmail.com>
2025-06-01 16:26:49 +0530

    commit

--- A-Z Pattern/H.cpp
+++ A-Z Pattern/H.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int height = 7;  
+    int width = 7;   
+    for (int i = 0; i < height; i++) {
+        // Left vertical line
+        cout << "*";
+
+        // Spaces or horizontal line in the middle
+        for (int j = 1; j < width - 1; j++) {
+            if (i == height / 2) {
+                // Middle horizontal line
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+
+        // Right vertical line
+        cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
 }
